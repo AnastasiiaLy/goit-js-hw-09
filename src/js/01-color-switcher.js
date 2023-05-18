75a95cd4999a995ae0c4644c3be79342ba01c2b5
Anastasiia Lytus <litusnastja@gmail.com>
2023-05-18 22:26:31 +0300

    1/3 of the homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+startBtn.disabled = false;
+
+const DELAY = 1000;
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  intervalSetup();
+  startBtn.disabled = true;
+}
+
+function intervalSetup() {
+  timerId = setInterval(() => {
+    onChangeBackgroundColor();
+  }, DELAY);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onChangeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
+
+function onStopBtnClick() {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
