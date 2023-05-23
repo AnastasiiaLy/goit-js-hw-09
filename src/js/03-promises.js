8a87ede0870ff899e4a4d3720243f08ef9c60547
Anastasiia Lytus <litusnastja@gmail.com>
2023-05-23 14:23:51 +0300

    3/3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,23 +5,24 @@ const stepInput = document.querySelector('input[name="step"]');
 const amountInput = document.querySelector('input[name="amount"]');
 
 const form = document.querySelector('.form');
-let delay = parseInt(delayInput.value);
 
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
+  let delay = parseInt(delayInput.value);
   const step = parseInt(stepInput.value);
   const amount = parseInt(amountInput.value);
 
   if (delay < 0 || step < 0 || amount <= 0) {
-    Notify.failure('These values should be higher than 0');
+    Notify.failure('Amount should be set up higher than 0');
   }
 
   {
     for (let i = 1; i <= amount; i += 1) {
       const position = i;
+      delay += step;
 
       createPromise(position, delay)
         .then(({ position, delay }) => {
@@ -30,7 +31,6 @@ function handleSubmit(event) {
         .catch(({ position, delay }) => {
           Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
         });
-      delay += step;
     }
   }
 }
