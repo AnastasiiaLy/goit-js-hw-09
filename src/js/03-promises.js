8ae537f6119dcc77210c4c92ee597a7af4ba6dbf
Anastasiia Lytus <litusnastja@gmail.com>
2023-05-23 14:03:15 +0300

    3/3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,27 +5,33 @@ const stepInput = document.querySelector('input[name="step"]');
 const amountInput = document.querySelector('input[name="amount"]');
 
 const form = document.querySelector('.form');
+let delay = parseInt(delayInput.value);
 
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  const initialDelay = parseInt(delayInput.value);
   const step = parseInt(stepInput.value);
   const amount = parseInt(amountInput.value);
 
-  for (let i = 1; i <= amount; i += 1) {
-    const position = i;
-    const delay = initialDelay + (i - 1) * step;
+  if (delay < 0 || step < 0 || amount <= 0) {
+    Notify.failure('These values should be higher than 0');
+  }
 
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
+  {
+    for (let i = 1; i <= amount; i += 1) {
+      const position = i;
+
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      delay += step;
+    }
   }
 }
 
